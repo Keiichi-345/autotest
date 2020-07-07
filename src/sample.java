98ba8507bad2afc5a68dbc255ffff6e16dfdbdbd
Keiichi-345 <mental0650daiji@gmail.com>
2020-07-07 18:45:33 +0900

    2020.7.7

--- src/sample.java
+++ src/sample.java
@@ -1,6 +1,6 @@
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -19,34 +19,47 @@ public class sample {
           System.setProperty("webdriver.chrome.driver", "./exe/chromedriver.exe");
           WebDriver driver = new ChromeDriver();
 
-          driver.manage().window().maximize();
-          driver.get("http://www.google.com");
-          Thread.sleep(50);
+          try( ICsvListReader listReader = new CsvListReader( new FileReader("./data/kadai002.csv"), CsvPreference.STANDARD_PREFERENCE )) {
+              List<String> list = null; //list : CSV一行分の情報を入れる
+              ArrayList<List<String>> lists = new ArrayList<List<String>>();//lists : それぞれの行ごとのlistをまとめる
 
-
-
-
-
-          try(ICsvListReader listReader = new CsvListReader(new FileReader("./data/kadi002.csv"), CsvPreference.STANDARD_PREFERENCE)) {
-              List<String> list = null;
-              for(Iterator it = list.iterator(); it.hasNext();) {
-                  System.out.println(it.next());
+              while( listReader.read() != null ) { //CSVの一行分の情報をlistにいれてそれをlistsにも入れる。最終行まで繰り返す。
+            	  list = listReader.read();
+            	  lists.add(list);
               }
-              while ((list = listReader.read()) != null) ;
+
+              //listsとlistの内容を確認
+              for( List<String> li : lists) {
+            	  System.out.println("------------------------------------------------------------------------------------");
+            	  System.out.println("・listの配列");
+            	  System.out.println(li);
+            	  System.out.println("");
+            	  System.out.println("・listの配列の要素");
+            	  for(String str : li) {
+            		  System.out.println(str);
+            	  }
+            	  System.out.println("------------------------------------------------------------------------------------");
+              }
+
+              //listsの一番目からテストを実行
+              for( int i = 0 ; i < lists.size() ; i++) {
+            	  List<String> nthList = lists.get(i);
+
+            	  driver.manage().window().maximize();
+                  driver.get(nthList.get(0));
+                  Thread.sleep(50);
+                  WebElement searchBox = driver.findElement(By.name(nthList.get(1)));
+                  searchBox.sendKeys(nthList.get(2));
+                  searchBox.submit();
+                  driver.findElement(By.xpath(nthList.get(3))).click();
+                  Thread.sleep(5000);
+                  driver.quit();
+              }
+
           } catch (IOException e) {
               throw new RuntimeException(e);
           }
 
-
-
-
-          WebElement searchBox = driver.findElement(By.name("q"));
-          searchBox.sendKeys("IT media");
-          searchBox.submit();
-
-          driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[9]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/a")).click();
-          Thread.sleep(5000);
-          driver.quit();
     }
 
 }
